6c4fe006aad639165e7d706af83d71322072c25e
christian83182 <christian.uaf@gmail.com>
2019-11-28 13:49:54 +0000

    Added folder for run results and correct results. Labelled 600/3000 testing images. Finished Run1.

--- src/main/java/uk/ac/soton/ecs/Run1.java
+++ src/main/java/uk/ac/soton/ecs/Run1.java
@@ -1,5 +1,6 @@
 package uk.ac.soton.ecs;
 
+import ch.akuhn.matrix.Vector;
 import org.openimaj.data.dataset.VFSGroupDataset;
 import org.openimaj.data.dataset.VFSListDataset;
 import org.openimaj.image.FImage;
@@ -8,6 +9,7 @@ import org.openimaj.image.processing.resize.ResizeProcessor;
 import org.openimaj.knn.DoubleNearestNeighboursExact;
 import org.openimaj.util.pair.IntDoublePair;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -20,29 +22,48 @@ import java.util.Map;
 
 public class Run1 {
     public static void main(String[] args) throws IOException {
+        Path trainingDataPath = Paths.get("resources/training/");
+        Path testingDataPath = Paths.get("resources/testing/");
+        VFSGroupDataset<FImage> trainingData = new VFSGroupDataset<>(trainingDataPath.toAbsolutePath().toString(), ImageUtilities.FIMAGE_READER);
+        VFSListDataset<FImage> testingData = new VFSListDataset<>(testingDataPath.toAbsolutePath().toString(), ImageUtilities.FIMAGE_READER);
+        Integer k =20; //current optimum.
 
-        //Specify the paths for the training and testing data. Useful since location will be relative.
-        Path trainingDataPath = Paths.get("resources/training/").toAbsolutePath();
-        Path testingDataPath = Paths.get("resources/testing/").toAbsolutePath();
+        Map<Integer,Double> results = new HashMap<>();
+        for(int i =1; i <= 20; i++ ){
+            runAlgorithm(trainingData,testingData, i*10);
+            Double accuracy = Utils.computeAccuracy(Paths.get("resources/results/correct.txt"), Paths.get("resources/results/run1.txt"));
+            results.put(i*10, accuracy);
+            System.out.println("Fininshed k=" + i*10 +"Accuracy= " + accuracy);
+        }
 
-        //Create datasets for the training and testing data using their respective paths.
-        VFSGroupDataset<FImage> trainingData = new VFSGroupDataset<>(trainingDataPath.toString(), ImageUtilities.FIMAGE_READER);
-        VFSListDataset<FImage> testingData = new VFSListDataset<>(testingDataPath.toString(), ImageUtilities.FIMAGE_READER);
+        for(Map.Entry<Integer,Double> entry : results.entrySet()){
+            System.out.println("K Value: " + entry.getKey() +"\tAccuracy: " +entry.getValue());
+        }
 
+        System.out.println();
+    }
+
+    /**
+     * A method which runs a simple Scene Classification algorithm. The algorithm resizes every training image to a 16x16 image, transforms this
+     * into a vector and then mean-centers and normalizes it. The same operation is done for all testing images, who's classification is determined by
+     * a weighted-vote of it's k-nearest-neighbours.
+     * @param trainingData The data which should be used for training the algorithm. It's assumed this is already classified.
+     * @param testingData The data which should be used to test the algorithm.
+     * @param k The value to be used as k in the k-nearest-neighbours algorithm.
+     * @throws IOException If writing to the file fails.
+     */
+    public static void runAlgorithm(VFSGroupDataset<FImage> trainingData, VFSListDataset<FImage> testingData, Integer k) throws IOException {
         //Create a print writer to output the data to a file.
-        PrintWriter writer = new PrintWriter(new FileWriter("run1.txt"));
+        File outputFile = new File("resources/results/run1.txt");
+        PrintWriter writer = new PrintWriter(new FileWriter(outputFile));
 
         //Create a map to store the 'tiny image' vectors and their respective categories.
         Map<double[],String> vectorMap = new HashMap<>();
-
-        //The resize processor is used to resize images to a 16x16 area. Aspect ratio is not maintained.
         ResizeProcessor resizeProcessor = new ResizeProcessor(16,16,false);
 
-        //Iterate over each image in each directory.
+        //Loop over each image in each directory and add their vector representation to the vectorMap.
         trainingData.forEach((directory, imageDataset) -> imageDataset.forEach(trainingImage -> {
-            //Resize the image and add it's 256-dimensional vector to the map, pointing to its respective classification.
-            resizeProcessor.processImage(trainingImage);
-            double[] trainingVector = trainingImage.getDoublePixelVector();
+            double[] trainingVector = resizeAndGetVector(resizeProcessor, trainingImage);
             vectorMap.put(trainingVector,directory);
         }));
 
@@ -55,29 +76,21 @@ public class Run1 {
             FImage testImage = testingData.get(testImageCounter);
 
             //Resize the test image and flatten it to a 256-dimensional vector.
-            resizeProcessor.processImage(testImage);
-            double[] testVector = testImage.getDoublePixelVector();
+            double[] testVector = resizeAndGetVector(resizeProcessor, testImage);
 
             //Create a Map which will be used to store the weighted vote for the classification of the point.
             Map<String, Double> voteMap = new HashMap<>();
+            List<IntDoublePair> neighbours = knn.searchKNN(testVector, k);
 
-            //Use the DoubleNearestNeighbourExact object to find the k nearest neighbours.
-            List<IntDoublePair> neighbours = knn.searchKNN(testVector, 5);
-
-            //Iterate over evey neighbour.
+            //Iterate over evey neighbour and add its vote to the VoteMap. An inverse distance function is used as the weight function.
             for(IntDoublePair indexAndDistance : neighbours){
-                //Get the neighbour's classification from it's index in the list of vectors given to knn.
                 String neighbourClassification = vectorMap.get(vectors[indexAndDistance.first]);
-                //compute the weight using a kernel (reciprocal distance in this case).
                 Double weightedVote = 1 / indexAndDistance.second;
-                //Add the weightedVote to the relevant classification in voteMap.
                 voteMap.put(neighbourClassification, voteMap.getOrDefault(neighbourClassification, 0.0) + weightedVote);
             }
 
-            //use Collections.max() to find the highest voted classification
+            //Iterate over all votes in the voteMap. and write to the file the highest-voted classification.
             Double maxDistance = Collections.max(voteMap.values());
-
-            //Iterate over all votes in the voteMap.
             for (String classification: voteMap.keySet()) {
                 if (voteMap.get(classification).equals(maxDistance)){
                     writer.println(testImageCounter +".jpg " + classification);
@@ -85,7 +98,29 @@ public class Run1 {
             }
         }
 
-        //Close the writer used to output to the file.
         writer.close();
+    }
+
+    /**
+     * A static method which resize an image using a given FImage and ResizeProcessor, then mean-centers the vector, normalizes the vector, and returns the result.
+     * @param resizeProcessor The ResizeProcessor which should be used when resizing the image (Recommended: 16x16 without aspect-ratio preservation).
+     * @param image The image from which the resultant vector should be based on.
+     * @return The resultant double[] representing an n-dimensional vector.
+     */
+    private static double[] resizeAndGetVector(ResizeProcessor resizeProcessor, FImage image){
+        resizeProcessor.processImage(image);
+        return centerAndNormalize(image.getDoublePixelVector());
+    }
+
+    /**
+     * A static method which mean-centers and normalizes a vector.
+     * @param vectorArray The vector array to be mean-centered and normalized
+     * @return The resultant n-dimensional vector in the form of a double[].
+     */
+    private static double[] centerAndNormalize(double[] vectorArray){
+        Vector vector = Vector.wrap(vectorArray);
+        vector.applyCentering();
+        vector.times(1/vector.norm());
+        return vector.unwrap();
     }
 }
