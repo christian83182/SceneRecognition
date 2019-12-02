c1e95f598dde3e589872de1c0711a9cdfa278820
Signe Rebassoo <signe.rebassoo@hotmail.com>
2019-12-02 22:04:25 +0000

    Created patching mathod.

--- src/main/java/uk/ac/soton/ecs/Run2.java
+++ src/main/java/uk/ac/soton/ecs/Run2.java
@@ -1,9 +1,12 @@
 package uk.ac.soton.ecs;
 
+import de.bwaldvogel.liblinear.SolverType;
 import org.openimaj.data.dataset.VFSGroupDataset;
 import org.openimaj.data.dataset.VFSListDataset;
 import org.openimaj.image.FImage;
 import org.openimaj.image.ImageUtilities;
+import org.openimaj.ml.annotation.linear.LiblinearAnnotator;
+import org.openimaj.ml.clustering.kmeans.FloatKMeans;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -11,6 +14,8 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Run2 {
     public static void main(String[] args) throws IOException {
@@ -30,8 +35,42 @@ public class Run2 {
         File outputFile = new File("resources/results/run1.txt");
         PrintWriter writer = new PrintWriter(new FileWriter(outputFile));
 
+        //LiblinearAnnotator lla = new LiblinearAnnotator(, LiblinearAnnotator.Mode.MULTICLASS, SolverType.L1R_LR,
+        //        1.0, 0.0, 1.0, true);
 
+        FloatKMeans cluster = FloatKMeans.createExact(500);
 
         writer.close();
+    }
+
+    /**
+     * Samples a list of 8x8 patches for an image every 4 pixels in the x and y direction.
+     * @param img The image to be sampled.
+     * @return List of 8x8 patches.
+     */
+    private static List<float[][]> getPatches(FImage img){
+
+        float[][] pixels = img.pixels;
+        List<float[][]> patches = new ArrayList<>();
+
+        // Sample every 4 pixels
+        for(int r = 0; r < pixels.length; r += 4){
+            for(int c = 0; c < pixels[c].length; c += 4){
+
+                float[][] patch = new float[8][8];
+
+                // Populate patch with pixels
+                for(int i = 0; i < patch.length; i++){
+                    for(int j = 0; j < patch[i].length; j++){
+
+                        if(r+i < pixels.length && c+j < pixels[i].length){
+                            patch[i][j] = pixels[r+i][c+j];
+                        }
+                    }
+                }
+            }
+        }
+
+        return patches;
     }
 }
